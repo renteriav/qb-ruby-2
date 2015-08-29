7c90eb17bee3a0116d42297174db1c8dd319aeda
Francisco Renteria <renteriav@hotmail.com>
2015-08-29 00:26:23 -0700

    added mailer to sales receipt

--- lib/quickbooks/service/sales_receipt.rb
+++ lib/quickbooks/service/sales_receipt.rb
@@ -5,6 +5,17 @@ module Quickbooks
       def delete(sales_receipt)
         delete_by_query_string(sales_receipt)
       end
+      
+      def mail(sales_receipt_id, email_address=nil)
+          query = email_address.present? ? "?sendTo=#{email_address}" : ""
+          url = "#{url_for_resource(model::REST_RESOURCE)}/#{sales_receipt_id}/send#{query}"
+          response = do_http_post(url,{})
+          if response.code.to_i == 200
+              model.from_xml(parse_singular_entity_response(model, response.plain_body))
+              else
+              nil
+          end
+      end
 
       private
 
